02ae65ec43c698cef23e5003d81955157a69c081
maggicoder <156202777+MaggiCoder16@users.noreply.github.com>
2025-07-24 21:04:24 +0530

    Update game.py

--- game.py
+++ game.py
@@ -23,7 +23,7 @@ class Game:
         info = Game_Information.from_gameFull_event(await game_stream_queue.get())
         opponent_is_bot = (info.black_title == 'BOT') if info.white_name == self.username else (info.white_title == 'BOT')
         if not opponent_is_bot:
-            self.config.opening_books.books.names = {
+            self.config.opening_books['books'] = {
                 "HumanBook": "./engines/Titans.bin"
             }
      
